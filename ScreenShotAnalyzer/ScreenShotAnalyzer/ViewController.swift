e7a35b63107805e1cfe12e5856d090b19551d2f6
yusufansar <yusufansar313@gmail.com>
2023-09-27 04:01:27 +0530

    intial app template.

---
+++ ScreenShotAnalyzer/ScreenShotAnalyzer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScreenShotAnalyzer
+//
+//  Created by maple on 27/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
