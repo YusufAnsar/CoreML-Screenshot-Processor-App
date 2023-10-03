630eaec5146abc79132cef5fa2dd6715094c97fd
yusufansar <yusufansar313@gmail.com>
2023-10-03 16:36:02 +0530

    Handled keyboard notification in screenshot detail screen.

--- ScreenShotAnalyzer/ScreenShotAnalyzer/ScreenshotDetailsViewController.swift
+++ ScreenShotAnalyzer/ScreenShotAnalyzer/ScreenshotDetailsViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ScreenshotDetailsViewController: UIViewController {
 
+    @IBOutlet private weak var scrollView: UIScrollView!
     @IBOutlet private weak var imageView: UIImageView!
     @IBOutlet private weak var noteTextField: UITextField!
     @IBOutlet private weak var creationDateLabel: UILabel!
@@ -44,10 +45,37 @@ class ScreenshotDetailsViewController: UIViewController {
         nameLabel.text = viewModel.getName()
         sizeLabel.text = viewModel.getSizeInfo()
         predictionsLabel.text = viewModel.getPredictionText()
+
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
     }
 
     @objc private func textFieldDidChange(_ textField: UITextField) {
         viewModel.update(screenshotNote: textField.text)
+    }
+
+    @objc func adjustForKeyboard(notification: Notification) {
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            scrollView.contentInset = .zero
+        } else {
+            scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+        }
+
+        scrollView.scrollIndicatorInsets = scrollView.contentInset
+        // If active text field is hidden by keyboard, scroll it so it's visible
+        // Your application might not need or want this behavior.
+        var scrollRect = self.view.frame;
+        scrollRect.size.height -= keyboardViewEndFrame.height
+        if !scrollRect.contains(noteTextField.frame.origin) {
+            let scrollPoint = CGPoint(x: 0, y: noteTextField.frame.origin.y - keyboardScreenEndFrame.height)
+            scrollView.setContentOffset(scrollPoint, animated: true)
+        }
     }
 }
 
